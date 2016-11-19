5f32ce73672a2e98c0981003bc1b59aea647225c
Tommy Tracy II <tjt7a@virginia.edu>
2016-11-19 14:38:16 -0500

    Updated RF code

--- src/automatize.py
+++ src/automatize.py
@@ -1,6 +1,8 @@
 '''
     The purpose of this program is to convert SKLEARN models into 
     an automata representation.
+
+    For the time being let's only support Random Forests
     ----------------------
     Author: Tom Tracy II
     email: tjt7a@virginia.edu
@@ -10,12 +12,145 @@
     Version 1.0
 '''
 
+
 # Support Imports
 from optparse import OptionParser
+import logging
+import chain
+import pickle
+import numpy as np
 
 # SKLEARN inputs
 
 # Micron AP import
+
+# Turn on logging.
+logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.INFO)
+ 
+# Load the model from a file
+def load_model(modelfile):
+    f = open(modelfile, 'rb')
+    model = pickle.load(f)
+    f.close()
+    return model
+
+
+'''
+    Attributes of an SKLEARN Tree -- returned by dir(tree)
+
+    ['__class__', '__delattr__', '__doc__', '__format__',
+     '__getattribute__', '__getstate__', '__hash__', '__init__', 
+     '__new__', '__pyx_vtable__', '__reduce__', '__reduce_ex__', 
+     '__repr__', '__setattr__', '__setstate__', '__sizeof__', '__str__',
+      '__subclasshook__', 'apply', 'capacity', 'children_left', 
+      'children_right', 'compute_feature_importances', 'decision_path', 
+      'feature', 'impurity', 'max_depth', 'max_n_classes', 'n_classes', '
+      n_features', 'n_node_samples', 'n_outputs', 'node_count', 'predict', 
+      'threshold', 'value', 'weighted_n_node_samples']
+
+'''
+
+# Convert tree to chains
+def tree_to_chains(tree, chains, features, threshold_map):
+
+    # Return a list of chains
+    #chains = []
+
+    # Return a list of features used
+    #features = []
+
+    # Return dictionary mapping features to all thresholds
+    #threshold_map = {}
+
+    # Root node attributes
+    feature = tree.feature[0]
+    threshold = tree.threshold[0]
+
+    # Keeping track of features and associated thresholds
+    if feature not in features:
+        features.append(feature)
+        threshold_map[feature] = [threshold]
+
+    elif threshold not in threshold_map[feature]:
+        threshold_map[feature].append(threshold)
+
+
+    value = np.argmax(tree.value[0])
+    left = tree.children_left[0]
+    right = tree.children_right[0]
+
+    left_chain = chain.Chain()
+    node = chain.Node(feature, threshold, value, False, False)
+    left_chain.add_node(node)
+    left_chain.set_start(node)
+    chains += recurse(tree, left, left_chain, features, threshold_map)
+
+    right_chain = chain.Chain()
+    node = chain.Node(feature, threshold, value, False, True)
+    right_chain.add_node(node)
+    right_chain.set_start(node)
+    chains += recurse(tree, right, right_chain, features, threshold_map)
+
+# Recursive function to convert trees into chains
+def recurse(tree, index, temp_chain, features, threshold_map):
+
+    feature = tree.feature[index]
+    threshold = tree.threshold[index]
+    value = np.argmax(tree.value[index])
+
+    left = tree.children_left[index]
+    right = tree.children_right[index]
+
+    leaf = (feature == -2)          # We're a leaf (also an end)
+
+    if leaf:                        # It's a leaf
+
+        node = chain.Node(feature, threshold, value, True, False)
+        temp_chain.add_edge(temp_chain.nodes_[-1], node)
+        temp_chain.set_end(node)
+
+        return [temp_chain]
+
+    else:                           # We can do this because all trees with children have both
+
+        # Keeping track of features and associated thresholds
+        if feature not in features:
+            features.append(feature)
+            threshold_map[feature] = [threshold]
+
+        elif threshold not in threshold_map[feature]:
+            threshold_map[feature].append(threshold)
+
+        node = chain.Node(feature, threshold, value, leaf, False)
+        temp_chain.add_edge(temp_chain.nodes_[-1], node)
+        right_copy = temp_chain.copy()
+
+        left_chains =  recurse(tree, left, temp_chain, features, threshold_map)
+
+        right_copy.nodes_[-1].set_direction(True)
+
+        right_chains = recurse(tree, right, right_copy, features, threshold_map)
+
+        return left_chains + right_chains
+
+# Set the character sets of each node in the chains
+def set_character_sets(chain, features, threshold_map):
+
+    for node in chain.nodes_:
+        feature = node.compute_
+        threshold = node.threshold_
+        gt = node.gt_
+
+        feature_range = threshold_map[feature]
+
+        # We're at a <= node
+        if not gt:
+            min_symbol = 0      # The left-most symbol is always accepted by a <= node
+
+        else:
+            max_symbol = 
+
+
 
 # Main()
 if __name__ == '__main__':
@@ -23,8 +158,42 @@ if __name__ == '__main__':
     # Parse Command Line Arguments
     usage = '%prog [options][text]'
     parser = OptionParser(usage)
-    parser.add_option('-m', '--model', type='string', dest='model', help='Input SKLEARN model')
+    parser.add_option('-m', '--model', type='string', dest='model', help='Input SKLEARN model pickle file')
     parser.add_option('-a', '--anml', type='string', dest='anml', default='model.anml', help='ANML output filename')
     parser.add_option('-v', '--verbose', action='store_true', default=False, dest='verbose', help='Verbose')
     options, args = parser.parse_args()
+
+    # Load the model file
+    if options.model is not None:
+        model = load_model(options.model)
+    else:
+        raise ValueError("No valid model file; provide -m <model filename>")
+        exit(-1)
+
+    # Grab the constituent trees
+    trees = [dtc.tree_ for dtc in  model.estimators_]
+
+    # Convert all trees to chains
+    chains = []
+
+    # Keep track of all features used in the forest
+    features = []
+
+    # Keep track of features to all thresholds
+    threshold_map = {}
+
+    # Iterate through all trees in the forest and keep track of chains, features, and thresholds
+    for tree in trees:
+        tree_to_chains(tree, chains, features, threshold_map)
+
+    # Sort the features
+    features.sort()
+
+    # For each set of thresholds, sort them
+    for k, v in threshold_map.items():
+        v.sort()
+
+    # Set the character sets for each node in the chains
+    for chain in chains:
+        set_character_sets(chain, features, threshold_map)
 
--- src/chain.py
+++ src/chain.py
@@ -1,5 +1,5 @@
 '''
-    This module defines a chain class
+    This objected-oriented module defines a chain class
     ----------------------
     Author: Tom Tracy II
     email: tjt7a@virginia.edu
@@ -9,50 +9,94 @@
     Version 1.0
 '''
 
+import copy
+
 # Define Node class
 class Node(object):
 
-	def __init__(self, feature, threshold, gt):
-		self.index_ = None 					# Index of the node
+	def __init__(self, feature, threshold, value, leaf, gt):
+		self.index_ = -1 					# Index of the node
 		self.feature_ = feature 			# Index of the feature
 		self.threshold_ = threshold 		# Threshold
+		self.value_ = value 				# Value
+		self.leaf_ = leaf					# Leaf node?
 		self.gt_ = gt 						# greater-than?; else less-than
+
+		# Automata-related stuff
+		self.character_set = []				# Start with an empty set
+
+	def copy(self):
+		return copy.deepcopy(self)
 
 	def __str__(self):
 		string = ""
-		string += "[i:" + str(self.index_) + ","
-		string += "f:" + str(self.feature_)
-		if self.gt_:
-			string += ">"
+		if self.leaf_:
+			string += "<LEAF: %s>" % str(self.value_)
 		else:
-			string += "<="
-		string += str(self.threshold_) + ']'
+			string += "[i:%s,f:%d" % (str(self.index_), self.feature_)
+			if self.gt_:
+				string += ">"
+			else:
+				string += "<="
+			string += "%s]" % str(self.threshold_)
 		return string
 
+	def __eq__(self, other):
+		return self.index_ == other.index_ and \
+				self.feature_ == other.feature_ and \
+				self.threshold_ == other.threshold_ and \
+				self.value_ == other.value_ and \
+				self.leaf_ == other.leaf_ and \
+				self.gt_ == other.gt_
+
+	# Set the index
 	def set_index(self, index):
 		self.index_ = index
+
+	# Set the direction (is the direction greater-than?)
+	def set_direction(self, gt):
+		self.gt_ = gt
+
+	# Set the character set of the current node
+	def set_character_set(self, min, max):
+		for i in range(min_symbol, max_symbol+1):
+			self.character_set.append(i)
 
 # Define Chain class
 class Chain(object):
 
 	def __init__(self):
 
-		self.node_lookup_ = []				# List of nodes
-		self.nodes_ = {}					# Node -> {list of adjacent nodes}
-		self.start_ = None					# Start of the chain
-		self.end_ = None					# End of the chain
-		self.uid_ = 0
+		self.nodes_ = []							# List of nodes
+		self.children_ = []							# Node -> [list of adjacent nodes]
+		self.start_ = None							# Start of the chain
+		self.end_ = None							# End of the chain
+		self.uid_ = 0								# Unique id for the current node
+
+	def copy(self):
+		return copy.deepcopy(self)
 
 	def __str__(self):
-		string = ""
-		for key, values in self.nodes_.items():
-			if key == self.start_:
+		string = "uid: " + str(self.uid_) + '\n'
+		for index, childs in enumerate(self.children_):
+			if index == self.start_:
 				string += "(START): "
-			string += (str(key) + " -> " + str(values))
-			if key == self.end_:
+			string += str(self.nodes_[index]) + "(" + str(index) + ")"
+			string += " -> "
+			if len(childs) > 0:
+				string += str(self.nodes_[childs[0]]) + "(" + str(childs[0]) + ")"
+			else:
+				string += "(NONE)"
+			if index == self.end_:
 				string += ": (END)"
 			string +=  '\n'
 		return string
+
+	# Can't use a real generator here because then I cant pickle :(
+	def index_generator(self):
+		uid = self.uid_
+		self.uid_ += 1
+		return uid
 
 	# Set the start node of the chain
 	def set_start(self, node):
@@ -64,44 +108,52 @@ class Chain(object):
 
 	# Add a node to the chain
 	def add_node(self, node):
-		node.set_index(self.uid_)
-		self.uid_ += 1
-		self.node_lookup_.append(node)
-		self.nodes_[node.index_] = []
+		if node in self.nodes_:
+			return -1
+		else:
+			index = self.index_generator()
+			node.set_index(index)
+			self.nodes_.append(node)
+			self.children_.append([])
+
+			assert node.index_ == (len(self.nodes_) - 1), "Node.index != len(nodes) - 1"
+			assert node.index_ == (len(self.children_) - 1), "Node.index != len(children) - 1"
+
+			return 1
 
 
 	# Add an edge from node -> neighbor
 	def add_edge(self, node, neighbor):
-		if node.index_ is None:
+		if node not in self.nodes_:				# It hasn't been added to the chain yet
 			self.add_node(node)
-		if neighbor.index_ is None:
+		if neighbor not in self.nodes_:
 			self.add_node(neighbor)
 			
-		if neighbor.index_ in self.nodes_[node.index_]:
+		if neighbor.index_ in self.children_[node.index_]:
 			return -1	# Edge already exists
 		else:
-			self.nodes_[node.index_].append(neighbor.index_)
+			self.children_[node.index_].append(neighbor.index_)
+			return 1
 
 	# Remove node from chain
 	def remove_node(self, node):
-		if node.index_ not in self.nodes_:
+		if node not in self.nodes_:
 			return -1
 		else:
-			del self.nodes_[node.index_]
-			self.node_lookup_[node.index_] = None
-
-		try:
-			del self.nodes_[index]
-		except:
-			return -1						# No such node
+			del self.children_[node.index_]
+			self.nodes_[node.index_] = None
+			return 1
 
 	# Remove an edge from the chain
 	def remove_edge(self, node, neighbor):
-		if node.index_ in self.nodes_:
-			try:
-				self.nodes_[node.index_].remove(neighbor.index_)
-			except:
-				return -1					# No such edge
+		if node not in self.nodes_:
+			return -1
+		if neighbor not in self.nodes_:
+			return -1
+
+		if neighbor.index_ in self.children_[node.index_]:
+			self.children_[node.index_].remove(neighbor.index_)
+			return 1
 		else:
 			return -1						# No such edge
 
@@ -111,15 +163,19 @@ if __name__ == "__main__":
 	C = Chain()
 
 	for f in range(10):	
-		node = Node(f, 0.1*f, f%2)
+		node = Node(f, 0.1*f, f%2, 0, False)
 		C.add_node(node)
 		if f > 0:
-			C.add_edge(C.node_lookup_[-2], C.node_lookup_[-1])
+			C.add_edge(C.nodes_[-2], C.nodes_[-1])
 
-	C.set_start(C.node_lookup_[0])
-	C.set_end(C.node_lookup_[-1])
-	C.remove_edge(C.node_lookup_[2], C.node_lookup_[3])
+	C.set_start(C.nodes_[0])
+	C.set_end(C.nodes_[-1])
+
+	D = C.copy()
 
 	print(C)
-	for n in C.node_lookup_:
-		print n
+
+	node = Node(10, 0.1*10, 0, 0, True)
+	D.add_edge(D.nodes_[-1], node)
+
+	print(D)
